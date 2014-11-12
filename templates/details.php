5901b63259781ad92dbf86c5f19a98b89a1e4e97
Marian <willarm@miamioh.edu>
2014-11-12 17:29:47 -0500

    Added details template to templates and corresponding css to the justified-nav file.

---
+++ templates/details.php
@@ -0,0 +1,32 @@
+<div class="details">
+	<div id="detailsContent">
+		<div id="detailsLeftCol"> <img src="http://placehold.it/150x150" alt="Sample App"></div>
+		<div id="detailsCenter">
+			  <h2>Sample App</h2>
+			  <p>Developer: Developer</p>
+			  <p>
+				<img src="assets/img/star_full.png" alt="Star">
+				<img src="assets/img/star_full.png" alt="Star">
+				<img src="assets/img/star_full.png" alt="Star">
+				<img src="assets/img/star_half.png" alt="Half Star">
+				<img src="assets/img/star_none.png" alt="Empty Star">
+				(Num reveiws)
+			  </p>
+			  <p>Platforms: mac, pc, linux</p>
+		</div>
+		<div id="rightCol">
+			<p>$5.00</p>
+			<p><a class="btn btn-primary" href="#" role="button">Go to Store &raquo;</a></p>
+		  </div>
+	</div>
+	<div id="detailsLowerContent">
+	  <h3>App Description</h3>
+	  <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo.</p>
+	  <div id="reviews">
+		<h3>Customer Reviews</h3>
+		<p>By Name on November 12, 2014</p>
+		<p>Platform: PC</p>
+		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+	  </div>
+	</div>
+</div>
