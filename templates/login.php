22c139c90bdb4f73d593a1ef0007e372b316bb92
Nick Venenga <venengnj@miamioh.edu>
2014-11-12 22:15:07 -0500

    Changed login form to post and added name to login button for easier processing

--- templates/login.php
+++ templates/login.php
@@ -1,14 +1,14 @@
 <div class="row">
 	<div id="login-form">
 		<h2>Login</h2>
-		<form action="#" method="get">
+		<form action="#" method="post">
 			<label>Email Address: 
 			<input type="email" class="form-control" name="username" placeholder="Email address" autofocus></label>
 			<br>
 			<label>Password:
 			<input type="password" class="form-control" name="password" placeholder="Password"></label>
 			<br>
-			<input type="submit" class="btn btn-default" value="Login">
+			<input type="submit" class="btn btn-default" name="login" value="Login">
 		</form>
 	</div>
 </div>
