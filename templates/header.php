5aa96146545f08fa4fde74ba6bf1a4aee4be9c44
Nick Venenga <venengnj@miamioh.edu>
2014-11-13 10:09:40 -0500

    Removed Javascript files added by Bootstrap for debugging

--- templates/header.php
+++ templates/header.php
@@ -16,10 +16,6 @@
 	
     <!-- Custom styles for this template -->
     <link href="assets/css/justified-nav.css" rel="stylesheet">
-	
-    <!-- Just for debugging purposes. Don't actually copy these 2 lines! -->
-    <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
-    <script src="assets/js/ie-emulation-modes-warning.js"></script>
 
     <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
     <!--[if lt IE 9]>
