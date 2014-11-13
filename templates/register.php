9fa96c6196786cdd0661d1794a5e02a8240be2ac
Brett <tomckob@miamioh.edu>
2014-11-12 19:42:33 -0500

    created register page skeleton

---
+++ templates/register.php
@@ -0,0 +1,17 @@
+<div class="row">
+	<div id="login-form">
+		<h2>Register</h2>
+		<form action="#" method="post">
+			<label>Email Address: 
+			<input type="email" class="form-control" name="username" placeholder="Email address" autofocus></label>
+			<br>
+			<label>Password:
+			<input type="password" class="form-control" name="password" placeholder="Password"></label>
+			<br>
+			<label>Confirm Password:
+			<input type="password" class="form-control" name="confirmPassword" placeholder="Password"></label>
+			<br>
+			<input type="submit" class="btn btn-default" value="Login">
+		</form>
+	</div>
+</div>
