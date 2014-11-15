084b2ad7dd0b723ec898437552053e975858776c
werleyst <werleyst@miamioh.edu>
2014-11-15 10:51:44 -0500

    added new file for html apps by developer

---
+++ templates/appsByDeveloper.php
@@ -0,0 +1,25 @@
+<div class="details">
+	<div id="detailsContent">
+		<div id="detailsCenter">
+			  <h2>Apps created by "Developer"</h2
+				<ul>
+					<li><h3>App name 1</h3>
+					<p>Rating: 4</p>
+					<p>Description: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+					</li>
+					<li><h3>App name 2</h3>
+					<p>Rating: 3</p>
+					<p>Description: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+					</li>
+					<li><h3>App name 3</h3>
+					<p>Rating: 3.5</p>
+					<p>Description: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+					</li>
+				</ul>
+		</div>
+		<div id="detailsRightCol">
+			<p>Total Apps: 3</p>
+			<p>Overall rating: 4.2</p>
+		  </div>
+	</div>
+</div>
