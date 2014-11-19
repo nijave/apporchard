eadc4766d130b8729869d8eeec2a038e670da551
Nick Venenga <venengnj@miamioh.edu>
2014-11-18 21:27:47 -0500

    Started implementing Application class

--- classes/Application.php
+++ classes/Application.php
@@ -4,5 +4,25 @@
 */
 
 class Application {
-
+	private $title; //string
+	private $developer; //string
+	private $price; //double
+	private $compatible_apple; //boolean, compatible with Apple
+	private $compatible_android; //boolean, compatible with Android
+	private $compatible_windows; //boolean, compatible with Windows
+	private $link_apple; //string, link to Apple app
+	private $link_android; //string, link to Android app
+	private $link_windows; //string, link to Windows app
+	private $link_developer; //link to developer website
+	private $keywords; //array[string] of keywords
+	private $description; //string, description of app
+	private $moderation_state; //string (pending, active, deleted), current moderation state of app
+	
+	public function __construct(String $title) {
+		$this->title = $title;
+	}
+	
+	public function setDeveloper(String $developer) {
+		$this->developer = $developer;
+	}
 }
