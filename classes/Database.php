7fb3c8aba9c62ac493b7c088f01013eefc1dec21
Nick Venenga <venengnj@miamioh.edu>
2014-11-19 10:59:20 -0500

    Created basic singleton

--- classes/Database.php
+++ classes/Database.php
@@ -8,6 +8,9 @@ require_once('../libraries/medoo/medoo.php');
 
 class Database extends medoo {
 
+    private static $instance = null;
+    
+    /*
 	// Overriding settings in medoo.php
 	//  -- Fields automatically populated by openshift environmental variables
 	protected $database_type = 'mysql';
@@ -17,6 +20,26 @@ class Database extends medoo {
 	protected $port = getenv('OPENSHIFT_MYSQL_DB_PORT');
 	protected $charset = 'utf8';
 	protected $database_name = getenv('OPENSHIFT_GEAR_NAME');
+*/
+    private function __construct() {
+        $instance = new medoo([
+            'database_type' => 'mysql',
+            'server' => getenv('OPENSHIFT_MYSQL_DB_HOST'),
+            'username' => getenv('OPENSHIFT_MYSQL_DB_USERNAME'),
+            'password' => getenv('OPENSHIFT_MYSQL_DB_PASSWORD'),
+            'port' => getenv('OPENSHIFT_MYSQL_DB_PORT'),
+            'database_name' => getenv('OPENSHIFT_GEAR_NAME'),
+            'charset' => 'utf8'
+        ]);
+    }
 
-
+    public static function getInstance() {
+        if($instance === null) {
+            $instance = new $this;
+        }
+        return $instance;
+    }
 }
+
+$db = Database::getInstance();
+var_dump($db);
