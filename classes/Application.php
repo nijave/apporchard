f7d0dad48f4694db5618a707efeac9ed8020f3ad
Nick Venenga <venengnj@miamioh.edu>
2014-11-18 22:17:28 -0500

    Added getters and setters

--- classes/Application.php
+++ classes/Application.php
@@ -4,6 +4,11 @@
 */
 
 class Application {
+    
+    const MODERATION_ACTIVE = "ACTIVE";
+    const MODERATION_PENDING = "PENDING";
+    const MODERATION_DELETED = "DELETED";
+    
 	private $title; //string
 	private $developer; //string
 	private $price; //double
@@ -18,11 +23,121 @@ class Application {
 	private $description; //string, description of app
 	private $moderation_state; //string (pending, active, deleted), current moderation state of app
 	
-	public function __construct(String $title) {
-		$this->title = $title;
+	public function __construct() {
+		$this->compatible_apple = false;
+        $this->compatible_android = false;
+        $this->compatible_windows = false;
+        $this->keywords = array();
 	}
 	
+    public function setTitle(String $title) {
+        $this->title = $title;
+    }    
+    
 	public function setDeveloper(String $developer) {
 		$this->developer = $developer;
 	}
+    
+    public function setPrice(double $price) {
+        $this->price = $price;
+    }
+    
+    private function changeCompatible(String $platform, boolean $value) {
+        switch($platform) {
+            case "Apple":
+                $this->compatible_apple = true;
+                break;
+            case "Android":
+                $this->compatible_android = true;
+                break;
+            case "Windows":
+                $this->compatible_windows = true;
+                break;
+            default:
+                throw new Exception("Invalid platform provided", 1);
+                break;
+        }
+    }
+    
+    public function setCompatible(String $platform) {
+        changeCompatible($platform, true);
+    }
+    
+    public function unsetCompatible(String $platform) {
+        changeCompatible($platform, false);
+    }
+    
+    public function setAppleLink(String $link) {
+        $this->link_apple = $link;
+    }
+    
+    public function setAndroidLink(String $link) {
+        $this->link_android = $link;
+    }
+    
+    public function setWindowsLink(String $link) {
+        $this->link_windows = $link;
+    }
+    
+    public function setKeywords($keywords) {
+        $this->keywords = $keywords;
+    }
+    
+    public function setDescription(String $desc) {
+        $this->description = $desc;
+    }
+    
+    public function setModerationState(String $state) {
+        $this->moderation_state = $state;
+    }
+    
+    public function getTitle() {
+        return $this->title;
+    }    
+    
+    public function getDeveloper() {
+        return $this->developer;
+    }
+    
+    public function getPrice() {
+        return $this->price;
+    }
+    
+    private function getCompatible(String $platform) {
+        switch($platform) {
+            case "Apple":
+                return $this->compatible_apple;
+            case "Android":
+                return $this->compatible_android;
+            case "Windows":
+                return $this->compatible_windows;
+            default:
+                throw new Exception("Invalid platform provided", 1);
+                break;
+        }
+    }
+    
+    public function getAppleLink() {
+        return $this->link_apple;
+    }
+    
+    public function getAndroidLink() {
+        return $this->link_android;
+    }
+    
+    public function getWindowsLink() {
+        return $this->link_windows;
+    }
+    
+    public function getKeywords() {
+        return $this->keywords;
+    }
+    
+    public function getDescription() {
+        return $this->description;
+    }
+    
+    public function getModerationState() {
+        return $this->moderation_state;
+    }
 }
