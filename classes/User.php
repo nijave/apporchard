0ebaf659e5c65b163e4f3ee81168968e67669b80
Nick Venenga <venengnj@miamioh.edu>
2014-11-19 22:53:30 -0500

    Fixed require statement path

--- classes/User.php
+++ classes/User.php
@@ -3,7 +3,7 @@
 	Class representation of each user
 */
 
-require_once('../libraries/uFlex/User.php');
+require_once('libraries/uFlex/User.php');
 
 class AO_User extends User { //AppOrchard user extends uFlex User
 
