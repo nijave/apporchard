6069008c389c214ed7a664dd261301a30b04480a
werleyst <werleyst@miamioh.edu>
2014-11-20 09:22:49 -0500

    create new approval web page

---
+++ templates/approval.php
@@ -0,0 +1,13 @@
+<div class="row">
+	<div id="Approval Form">
+		<h2>Admin New App Approval</h2>
+		<form action="/" method="post">
+			<h3>App name 1</h3>
+			<p>Developer: Developer</p>
+			<p>Description: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+			
+			<input type="submit" class="btn btn-default" name="action" value="Accept">
+			<input type="submit" class="btn btn-default" name="action" value="Decline">
+		</form>
+	</div>
+</div>
