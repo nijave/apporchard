3cc87c4bd61002844da9f4862f7f243db3dc7bad
Nick Venenga <venengnj@miamioh.edu>
2014-11-19 22:52:24 -0500

    Created an abstract class as a template for Form Views

---
+++ classes/Form_Action.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * This class is a template for Form Action handlers
+ */
+ 
+abstract class Form_Action {
+    private $requiredParams; //parameters required to complete the request
+    private $requestData; //request payload/data
+    private $object; //object created by class
+    
+    /**
+     * Construct object
+     * $request pass-by-reference $_REQUEST array
+     */
+    abstract public function __construct(&$request);
+    
+    /**
+     * Checks the parameters required against what was submitted
+     * @return boolean whether or not required parameters are set
+     */
+    abstract public function checkParams();
+    
+    /**
+     * Processes the data and creates an object representation based on the request
+     * @return object representation of the data
+     */
+     abstract public function processData();        
+}
+ 
