165b4e2eef1c003c8237503baea838daa0f70886
Nick Venenga <venengnj@miamioh.edu>
2014-12-01 14:39:23 -0500

    Cleaned up comments for better readability

--- classes/Database.php
+++ classes/Database.php
@@ -41,7 +41,6 @@ class Database {
             'link_android' => $app->getStoreLink("Android"),
             'link_windows' => $app->getStoreLink("Windows"),
             'developer_link' => $app->getDeveloperLink(),
-            //'keyword' => implode(',', $app->getKeywords()), //separate keywords with a comma
             'description' => $app->getDescription(),
             'moderation_state' => $app->getModerationState()
         );
@@ -59,24 +58,35 @@ class Database {
      * @return int ID of the application modified or inserted
      */
     public static function applicationSet(Application $app) {
-        self::setInstance(); //creates database connection if one doesn't already exist
+        //creates database connection if one doesn't already exist
+        self::setInstance();
         
-        $exists = self::$instance->select("applications", ["id"], ["id"=>$app->getID()]); //determine if application already exists in database
+        //determine if application already exists in database
+        $exists = self::$instance->select("applications", ["id"], ["id"=>$app->getID()]);
         
-        if (sizeof($exists) === 0) { //check to see if application already exists in database
+        //check to see if application already exists in database
+        if (sizeof($exists) === 0) {
+            //create new application if one doesn't exist
             $id = self::$instance->insert("applications", self::makeSQLArray($app));
-        } //create new application if one doesn't exist
+        } 
         else {
+            //update application if it already exists
             self::$instance->update("applications", self::makeSQLArray($app), ["id" => $app->getID()]);
             $id = $app->getID();
-        } //update application if it already exists
+        }
         
-        self::$instance->delete("keywords", ["id" => $id]); //remove all existing keywords
+        //remove all existing keywords
+        self::$instance->delete("keywords", ["id" => $id]);
+        
+        //create an array of the keywords to be inserted
         $keyword_array = array();
-        foreach($app->getKeywords() as $word) { //create an array of the keywords to be inserted
+        foreach($app->getKeywords() as $word) {
             $keyword_array[] = array("id" => $id, "word" => $word);
         }
-        self::$instance->insert("keywords", $keyword_array); //insert keywords into keyword table
+        
+        //insert keywords into keyword table
+        self::$instance->insert("keywords", $keyword_array);
+        
         return $id;
     }
     
@@ -87,26 +97,40 @@ class Database {
      * @return array of applications IDs ordered by hits
      */
     public static function applicationSearch($keywords) {
-        self::setInstance(); //creates database connection if one doesn't already exist
+        //creates database connection if one doesn't already exist
+        self::setInstance();
          
-        for($i = 0; $i < count($keywords); $i++) { //remove whitespace and escape keywords, surround in single quotes
+        //remove whitespace and escape keywords, surround in single quotes
+        for($i = 0; $i < count($keywords); $i++) {
             $keywords[$i] = "'" . (mysql_escape_string($keywords[$i])) . "'";
         }
-        $keyword_list = implode(',', $keywords); //create a comma separated list of keywords
+        
+        //create an array of LIKE SELECT statements for each word 
+        //-- this way the query will match titles if it's only part of the title
         $title_selects = array();
         foreach($keywords as $word) {
             $title_selects[] = "SELECT id, '5' as weight FROM applications WHERE title LIKE $word";
         }
+        
+        //create a comma separated list of keywords
+        $keyword_list = implode(',', $keywords);
+        
+        //generate query for search using parts from above
         $query =  "SELECT id FROM("
                     . "SELECT id, '2' as weight FROM keywords WHERE word IN ($keyword_list)"
                     . " UNION ALL "
                     . implode(" UNION ALL ", $title_selects)
                 . ") AS search_results GROUP BY id ORDER BY SUM(weight) DESC;";
+        
+        //get array of results
         $raw_results = self::$instance->query($query)->fetchAll(PDO::FETCH_ASSOC);
+        
+        //cleanup array so it only returns application ids
         $results = array();
         foreach($raw_results as $result) { //cleanup MySQL results and change into an array of ints
             $results[] = $result["id"];
         }
+        
         return $results;
     }
 }
