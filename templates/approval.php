b3b0ac82d4d92d2cb591e9b63b75c380cc240d95
michael mumma <mummamw@miamioh.edu>
2014-12-02 15:21:26 -0500

    added php loop

--- templates/approval.php
+++ templates/approval.php
@@ -1,13 +1,19 @@
 <div class="row">
 	<div id="Approval Form">
 		<h2>Admin New App Approval</h2>
+		
+		$apps = array(database::applicationGetPending());
+		
+		for ($i = 0; $x <= sizeof($apps); $i++) {
 		<form action="/" method="post">
-			<h3>App name 1</h3>
-			<p>Developer: Developer</p>
-			<p>Description: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
-			
+			<?php
+				<h3>echo database::applicationGet($apps($i))</h3>
+				<p>Developer: Developer</p>
+				<p>Description: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
+			?>
 			<input type="submit" class="btn btn-default" name="action" value="Accept">
 			<input type="submit" class="btn btn-default" name="action" value="Decline">
 		</form>
+		}
 	</div>
 </div>
