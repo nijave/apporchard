d9258752d97e245c0f6e56f7b2ab23190cf4a55d
Brett <tomckob@miamioh.edu>
2014-12-02 18:55:40 -0500

    fixed syntax errors

--- templates/approval.php
+++ templates/approval.php
@@ -2,18 +2,20 @@
 	<div id="Approval Form">
 		<h2>Admin New App Approval</h2>
 		
-		$apps = array(database::applicationGetPending());
+		<?php
+		$apps = applicationGetPending();
 		
-		for ($i = 0; $x <= sizeof($apps); $i++) {
-		<form action="/" method="post">
-			<?php
-				<h3>echo database::applicationGet($apps($i))->name;</h3>
+		for($i = 0; $i < $apps.size(); $i++) { ?>
+			<form action="/" method="post">
+				<h3><?php applicationGet($apps[$i]).title; ?></h3>
 				<p>Developer: Developer</p>
 				<p>Description: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
-			?>
-			<input type="submit" class="btn btn-default" name="action" value="Accept">
-			<input type="submit" class="btn btn-default" name="action" value="Decline">
-		</form>
+				
+				<input type="submit" class="btn btn-default" name="action" value="Accept">
+				<input type="submit" class="btn btn-default" name="action" value="Decline">
+			</form>
+		<?php 
 		}
+		?>
 	</div>
 </div>
