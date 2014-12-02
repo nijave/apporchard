e36b7dd1d035540aab1e74fada3d8e6d037951dd
Marian <willarm@miamioh.edu>
2014-12-02 16:57:55 -0500

    Fixed error in help template

--- templates/help.php
+++ templates/help.php
@@ -2,7 +2,8 @@
 	<?php
 	$help = fopen("docs/HELP.md", "r") or die("Unable to open file!");
 	// Output one line until end-of-file
-	echo "<h2>{fgets($help)}</h2>";
+	$welcome = fgets($help);
+	echo "<h2>{$welcome}</h2>";
 	while(!feof($help)) {
 	   echo fgets($help) . "<br>";
 	}
