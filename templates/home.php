c089875e393ceda41840a3ade043184a5ee0fea3
Nick Venenga <venengnj@miamioh.edu>
2014-12-03 11:04:46 -0500

    Updated app tile styling

--- templates/home.php
+++ templates/home.php
@@ -10,8 +10,14 @@
     
     foreach($app_ids as $id) {
         $app = new Application($id);
+        if(strlen($app->getDescription()) > 141) {
+            $desc = substr($app->getDescription(), 0, 138) . "&hellip;";
+        }
+        else {
+            $desc = $app->getDescription();
+        }
         echo '
-        <div class="col-xs-2">
+        <div class="col-xs-2 app-box">
           <img src="'.$app->getImageURL().'" alt="'.$app->getTitle().'">
           <h2>'.$app->getTitle().'</h2>
           <p>
@@ -21,7 +27,7 @@
                 <img src="assets/img/star_half.png" alt="Half Star">
                 <img src="assets/img/star_none.png" alt="Empty Star">
           </p>
-          <p>'.substr($app->getDescription(), 0, 100).'&hellip;</p>
+          <p class="app-desc">'.$desc.'&hellip;</p>
           <p><a class="btn btn-primary" href="/?page=details&id='.$app->getID().'" role="button">View details &raquo;</a></p>
         </div>
         ';
