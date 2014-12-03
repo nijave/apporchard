ef128b4a35e91a5bddcd50474de146119a60b37a
Brett <tomckob@miamioh.edu>
2014-12-02 19:42:32 -0500

    syntax errors

--- templates/approval.php
+++ templates/approval.php
@@ -7,7 +7,7 @@
 		
 		for($i = 0; $i < count($apps); $i++): ?>
 			<form action="/" method="post">
-				<h3><?php Database::applicationGet($apps[$i])->title; ?></h3>
+				<h3><?php Database::applicationGet($apps[$i])->getTitle(); ?></h3>
 				<p>Developer: Developer</p>
 				<p>Description: Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </p>
 				
