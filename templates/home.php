81d097e561befa4a2489e193a94838d4eef83f81
Nick Venenga <venengnj@miamioh.edu>
2014-12-03 10:44:45 -0500

    Finished homepage and categories pages

--- templates/home.php
+++ templates/home.php
@@ -2,27 +2,29 @@
 
     <?php
     if ($category === '') {
-        for ($i = 0; $i < 8; $i++)
-            echo '
-            <div class="col-xs-2">
-              <img src="//placehold.it/150x150" alt="Sample App">
-              <h2>Sample App</h2>
-              <p>
-                    <img src="assets/img/star_full.png" alt="Star">
-                    <img src="assets/img/star_full.png" alt="Star">
-                    <img src="assets/img/star_full.png" alt="Star">
-                    <img src="assets/img/star_half.png" alt="Half Star">
-                    <img src="assets/img/star_none.png" alt="Empty Star">
-              </p>
-              <p>Donec id elit non mi porta gravida at eget metus. Fusce dapibus, tellus ac cursus commodo.</p>
-              <p><a class="btn btn-primary" href="/?page=details" role="button">View details &raquo;</a></p>
-            </div>
-            ';
+        $app_ids = Database::applicationGetNewest();
     }
     else {
-        /*
-         * Display applications corresponding to $category
-         */
+        $app_ids = Database::applicationGetByCategory($category);
+    }
+    
+    foreach($app_ids as $id) {
+        $app = new Application($id);
+        echo '
+        <div class="col-xs-2">
+          <img src="'.$app->getImageURL().'" alt="'.$app->getTitle().'">
+          <h2>'.$app->getTitle().'</h2>
+          <p>
+                <img src="assets/img/star_full.png" alt="Star">
+                <img src="assets/img/star_full.png" alt="Star">
+                <img src="assets/img/star_full.png" alt="Star">
+                <img src="assets/img/star_half.png" alt="Half Star">
+                <img src="assets/img/star_none.png" alt="Empty Star">
+          </p>
+          <p>'.$app->getDescription().'</p>
+          <p><a class="btn btn-primary" href="/?page=details&id='.$app->getID().'" role="button">View details &raquo;</a></p>
+        </div>
+        ';
     }
     ?>
 
