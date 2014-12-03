3f4ef3b9c55f630f63ae870f6eb61f70e6b8da46
Nick Venenga <venengnj@miamioh.edu>
2014-12-03 11:05:05 -0500

    Updated app tile styling

--- templates/home.php
+++ templates/home.php
@@ -27,7 +27,7 @@
                 <img src="assets/img/star_half.png" alt="Half Star">
                 <img src="assets/img/star_none.png" alt="Empty Star">
           </p>
-          <p class="app-desc">'.$desc.'&hellip;</p>
+          <p class="app-desc">'.$desc.'</p>
           <p><a class="btn btn-primary" href="/?page=details&id='.$app->getID().'" role="button">View details &raquo;</a></p>
         </div>
         ';
