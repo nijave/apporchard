b9be6d7dff01cbbef2016787a876ce0a7370db6c
Brett <tomckob@miamioh.edu>
2014-12-02 19:04:03 -0500

    testing array size counters

--- templates/approval.php
+++ templates/approval.php
@@ -5,7 +5,7 @@
 		<?php
 		$apps = Database::applicationGetPending();
 		
-		for($i = 0; $i < $apps.length(); $i++) { ?>
+		for($i = 0; $i < $apps.count(); $i++) { ?>
 			<form action="/" method="post">
 				<h3><?php Database::applicationGet($apps[$i]).title; ?></h3>
 				<p>Developer: Developer</p>
