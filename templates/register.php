5a9b76e3a4835f3c0a57a141fdebfde77d368144
Nick Venenga <venengnj@miamioh.edu>
2014-12-03 21:29:23 -0500

    Fixed errors in register attempt 4

--- templates/register.php
+++ templates/register.php
@@ -3,16 +3,16 @@
 		<h2>Register</h2>
 		<form action="/" method="post">
 			<label>Username: 
-			<input type="text" class="form-control" name="username" placeholder="Username" autofocus></label>
+			<input type="text" class="form-control" name="Username" placeholder="Username" autofocus></label>
 			<br>
 			<label>Email Address: 
 			<input type="email" class="form-control" name="email" placeholder="Email address"></label>
 			<br>
 			<label>Password:
-			<input type="password" class="form-control" name="password" placeholder="Password"></label>
+			<input type="password" class="form-control" name="Password" placeholder="Password"></label>
 			<br>
 			<label>Confirm Password:
-			<input type="password" class="form-control" name="confirmPassword" placeholder="Password"></label>
+			<input type="password" class="form-control" name="Password2" placeholder="Password"></label>
 			<br>
 			<label>Account Type:
 			<select name="groupID">
