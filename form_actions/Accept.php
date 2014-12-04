57c9dd308bce8b924fa4f06c7eed0e383e20b5cc
Brett <tomckob@miamioh.edu>
2014-12-04 01:52:31 -0500

    fixed syntax

--- form_actions/Accept.php
+++ form_actions/Accept.php
@@ -1,24 +1,26 @@
 <div class="row">
-    <div id="Accept">
+    <div id="Decline">
+        <?php
 		if ($user->isSigned()) {
 			if($user->GroupID == 1 || $user->GroupID == 2){
 								echo "You do not have access to this page";
 			}
 			
-			else if (applicationGet($_POST[$id]).getModerationState() != "PENDING") {
+			else if (applicationGet($_POST["$id"]).getModerationState() != "PENDING") {
 								echo "This application is not up for approval"
 			}
 			
 			else {
-        <?php
-		applicationGet($_POST[$id])->setModerationState("ACTIVE");
+
+		applicationGet($_POST["id"])->setModerationState("ACTIVE");
 		save();
-		?>
+
 			}
 		}
 		
 		else{
 			echo 'You must be logged in to view this page';
 		}
+		?>
     </div>
 </div>
