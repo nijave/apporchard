6905e09e9f0902750d7cac277f8a7991814b52dc
Samuel Werley <werleyt@miamioh.edu>
2014-12-04 00:01:36 -0500

    added control based on user credentials

--- templates/approval.php
+++ templates/approval.php
@@ -1,21 +1,33 @@
 <div class="row">
 	<div id="Approval Form">
 		<h2>Admin New App Approval</h2>
-		
 		<?php
-		$apps = Database::applicationGetPending();
+			if ($user->isSigned()) {
+				if($user->GroupID == 1 || $user->GroupID == 2){
+				echo "<h2>You do not have acces to this page</h2>";
+				}
+				else{
+				echo'
+					<?php
+					$apps = Database::applicationGetPending();
 		
-		for($i = 0; $i < count($apps); $i++): ?>
-			<form action="/" method="post">
-				<h3><?php echo Database::applicationGet($apps[$i])->getTitle(); ?></h3>
-				<p><?php echo Database::applicationGet($apps[$i])->getDeveloper(); ?></p>
-				<p><?php echo Database::applicationGet($apps[$i])->getDescription(); ?></p>
+					for($i = 0; $i < count($apps); $i++): ?>
+						<form action="/" method="post">
+							<h3><?php echo Database::applicationGet($apps[$i])->getTitle(); ?></h3>
+							<p><?php echo Database::applicationGet($apps[$i])->getDeveloper(); ?></p>
+							<p><?php echo Database::applicationGet($apps[$i])->getDescription(); ?></p>
 				
-				<input type="submit" class="btn btn-default" name="action" value="Accept">
-				<input type="submit" class="btn btn-default" name="action" value="Decline">
-			</form>
-		<?php 
-		endfor;
-		?>
+							<input type="submit" class="btn btn-default" name="action" value="Accept">
+							<input type="submit" class="btn btn-default" name="action" value="Decline">
+						</form>
+					<?php 
+					endfor;
+					?>';				
+				}
+			}
+			else{
+				echo 'You must be logged in to view this page';
+			}
+		?>	
 	</div>
 </div>
