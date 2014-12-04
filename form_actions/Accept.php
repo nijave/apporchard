9d99bc5a6cc65ab8c60b85d382944ee8dba62d51
Brett <tomckob@miamioh.edu>
2014-12-04 01:45:51 -0500

    improved logic and syntax

--- form_actions/Accept.php
+++ form_actions/Accept.php
@@ -1,8 +1,24 @@
 <div class="row">
     <div id="Accept">
+		if ($user->isSigned()) {
+			if($user->GroupID == 1 || $user->GroupID == 2){
+								echo "You do not have access to this page";
+			}
+			
+			else if (applicationGet($_POST[$id]).getModerationState() != "PENDING") {
+								echo "This application is not up for approval"
+			}
+			
+			else {
         <?php
-		Database::setModerationState("ACTIVE");
+		applicationGet($_POST[$id])->setModerationState("ACTIVE");
 		save();
 		?>
+			}
+		}
+		
+		else{
+			echo 'You must be logged in to view this page';
+		}
     </div>
 </div>
