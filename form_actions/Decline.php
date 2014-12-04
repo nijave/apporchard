f2283e8b9fc720430d44218e160976eaa4f3bf7f
Nick Venenga <venengnj@miamioh.edu>
2014-12-04 08:09:06 -0500

    Tweaks and fixes

--- form_actions/Decline.php
+++ form_actions/Decline.php
@@ -1,13 +1,13 @@
 <div class="row">
     <div id="Decline">
         <?php
+        $a = Database::applicationGet($_POST["id"]);
         if ($user->isSigned()) {
             if ($user->GroupID == 1 || $user->GroupID == 2) {
                 echo "You do not have access to this page";
-            } else if (applicationGet($_POST["id"]) . getModerationState() != "PENDING") {
+            } else if ($a->getModerationState() !== "PENDING") {
                 echo "This application is not up for approval";
             } else {
-                $a = applicationGet($_POST["id"]);
                 $a->setModerationState("DELETED");
                 $a->save();
             }
