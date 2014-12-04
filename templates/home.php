59522299fc552d1c61a2d9bd9241ebd11bba5224
Nick Venenga <venengnj@miamioh.edu>
2014-12-04 10:42:16 -0500

    Stars removed from apps

--- templates/home.php
+++ templates/home.php
@@ -20,6 +20,7 @@
         <div class="col-xs-2 app-box">
           <img src="'.$app->getImageURL().'" alt="'.$app->getTitle().'">
           <h2>'.$app->getTitle().'</h2>
+		  <!-- This feature doens\'t exist yet
           <p>
                 <img src="assets/img/star_full.png" alt="Star">
                 <img src="assets/img/star_full.png" alt="Star">
@@ -27,6 +28,7 @@
                 <img src="assets/img/star_half.png" alt="Half Star">
                 <img src="assets/img/star_none.png" alt="Empty Star">
           </p>
+		  -->
           <p class="app-desc">'.$desc.'</p>
           <p><a class="btn btn-primary" href="/?page=details&id='.$app->getID().'" role="button">View details &raquo;</a></p>
         </div>
