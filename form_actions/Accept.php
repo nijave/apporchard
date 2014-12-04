3f54b2f54252e4893161563689d4c190bd7d288b
Nick Venenga <venengnj@miamioh.edu>
2014-12-04 08:05:29 -0500

    Tweaks and fixes

--- form_actions/Accept.php
+++ form_actions/Accept.php
@@ -1,26 +1,19 @@
 <div class="row">
     <div id="Decline">
         <?php
-		if ($user->isSigned()) {
-			if($user->GroupID == 1 || $user->GroupID == 2){
-								echo "You do not have access to this page";
-			}
-			
-			else if (applicationGet($_POST["$id"]).getModerationState() != "PENDING") {
-								echo "This application is not up for approval"
-			}
-			
-			else {
-
-		applicationGet($_POST["id"])->setModerationState("ACTIVE");
-		save();
-
-			}
-		}
-		
-		else{
-			echo 'You must be logged in to view this page';
-		}
-		?>
+        if ($user->isSigned()) {
+            if ($user->GroupID == 1 || $user->GroupID == 2) {
+                echo "You do not have access to this page";
+            } else if (applicationGet($_POST["id"]) . getModerationState() != "PENDING") {
+                echo "This application is not up for approval";
+            } else {
+                $a = applicationGet($_POST["id"]);
+                $a->setModerationState("ACTIVE");
+                $a->save();
+            }
+        } else {
+            echo 'You must be logged in to view this page';
+        }
+        ?>
     </div>
 </div>
