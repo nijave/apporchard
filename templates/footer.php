8d8f820853ab3c2c53c2414f4fc6232e2f2fcb5f
Nick Venenga <venengnj@miamioh.edu>
2014-12-07 20:43:45 -0500

    Updated footer

--- templates/footer.php
+++ templates/footer.php
@@ -1,4 +1,4 @@
 <!-- Site footer -->
 <div class="footer">
-	<p><a href="/?page=help">Help</a> | &copy; Red Brick Software 2014</p>
+	<p><a href="/?page=help">Help</a> | Copyright &copy; <?php echo date("Y"); ?> Red Brick Software</p>
 </div>
