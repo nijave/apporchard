347138078530a645dfc7fe3215b3b8221b1ae0b0
Nick Venenga <venengnj@miamioh.edu>
2014-12-07 20:58:18 -0500

    Added support for returning to the last page after logging in and out

--- templates/logout.php
+++ templates/logout.php
@@ -2,12 +2,12 @@
 	<div id="login-form">
 		<h2>Successfully logged out.</h2>
 		<?php
-			$user->logout();
+                    $user->logout();
+
+                    $return_uri = isset($_POST['return_uri']) ? filter_input(INPUT_POST, 'return_uri', FILTER_SANITIZE_STRING) : -1;
+                    if($return_uri !== -1) {
+                        header("Location: " . $return_uri);
+                    }
 		?>
 	</div>
-        <script type="text/javascript">
-            setTimeout(function () {
-                window.location.href = "/";
-            }, 2000);
-        </script>
 </div>
