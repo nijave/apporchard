7490e320b99110622ab12f61ddb3f04fe2ef7f6d
Nick Venenga <venengnj@miamioh.edu>
2014-12-13 17:20:49 -0500

    Separating and cleaning up PHP/HTML

--- templates/approval.php
+++ templates/approval.php
@@ -4,31 +4,35 @@
 		<?php
                 if ($user->isSigned()) {
                     if($user->GroupID < $user::MODERATOR){ ?>
-                        You do not have access to this page
+                        <span class="alert alert-danger">You do not have access to this page</span>
                     <?php }
                     else{
                         $apps = Database::applicationGetPending();
-
-                        for($i = 0; $i < count($apps); $i++):
-                            $a = new Application($apps[$i]);
-                            echo'<form action="/" method="post"><h3>'; 
-                            echo '<a href="/?page=details&id='.$a->getID().'">'.$a->getTitle().'</a>'; 
-                            echo '</h3><p>'; 
-                            echo $a->getDeveloper();
-                            echo '</p><p>';
-                            echo $a->getDescription(); 
-                            echo '</p>
+                        for($i = 0; $i < count($apps); $i++) {
+                            $a = new Application($apps[$i]); ?>
+                            <form action="/" method="post">
+                                <h3> 
+                                    <a href="/?page=details&id=.<?php echo $a->getID(); ?>">
+                                        <?php echo $a->getTitle(); ?>
+                                    </a>
+                                </h3>
+                                <p>
+                                    <?php echo $a->getDeveloper(); ?>
+                                </p>
+                                <p>
+                                    <?php echo $a->getDescription(); ?>
+                                </p>
                                 <input type="hidden" name="id" value="'.$a->getID().'">
                                 <input type="hidden" name="action" value="Approve">
                                 <input type="hidden" name="return" value="'. $uri .'">
                                 <input type="submit" class="btn btn-default" name="response" value="Accept">
                                 <input type="submit" class="btn btn-default" name="response" value="Decline">
-                            </form>';
-                        endfor;			
+                            </form>
+                        <?php }		
                     }
                 }
                 else { ?>
-                    You must be logged in to view this page
+                        <span class="alert alert-danger">You must be logged in to view this page</span>
                 <?php } ?>	
 	</div>
 </div>
